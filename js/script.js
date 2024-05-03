136588f20c4ae20f437b5d5a520937da17dbb43a
Alex Mwaura <67957197+AlexMwauraK@users.noreply.github.com>
2024-05-03 13:17:34 +0300

    update

--- js/script.js
+++ js/script.js
@@ -17,3 +17,29 @@ const navSlide = () => {
   });
 }
 navSlide();
+// Animations 
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+    else
+    {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.animate, .animate2, .animate3, .animate4');
+hiddenElements.forEach((el) => observer.observe(el));
+
+window.addEventListener('load', function () {
+const preloader = document.querySelector('.preloader');
+setTimeout(function () {
+preloader.style.opacity = '0';
+setTimeout(function () {
+  preloader.style.display = 'none';
+}, 500); // Adjust the delay as needed
+}, 1000); // Adjust the delay as needed
+}); 
